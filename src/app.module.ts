f375ebab68c0d34bafebcd1d2707582720aeecb1
Pedro Cauich <A19216289@alumnos.uady.mx>
2025-11-13 13:53:31 -0600

    base crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AlumnosModule } from './alumnos/alumnos.module';
+import { ProfesoresModule } from './profesores/profesores.module';
 
 @Module({
-  imports: [],
+  imports: [AlumnosModule, ProfesoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
